982b40931eb8c88d43a4ae61a35b32bea893105f
faizan102418 <m.faizansajid102418@gmail.com>
2026-07-17 16:33:48 +0500

    Stage 2: read endpoints with 404

--- week2_assingment/main.py
+++ week2_assingment/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+tasks = [
+    {"id": 1, "title": "Buy milk", "done": False},
+    {"id": 2, "title": "Complete Stage 2", "done": True},
+    {"id": 3, "title": "Commit to Git", "done": False},
+]
+
 @app.get("/")
 def read_root():
     return { 
@@ -15,3 +21,15 @@ def read_health():
     return   { 
         "status": "ok" 
 }
+    
+@app.get("/tasks")
+def read_tasks():
+    return tasks
+
+
+@app.get("/tasks/{id}")
+def read_task(id:int):
+    for task in tasks:
+        if task["id"] == id:
+            return task
+    return {"error": "Task not found"}
